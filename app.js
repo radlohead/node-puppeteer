b7f9a17de534432ecf25fc20a0b3393650e24e2a
= <radlohead@naver.com>
2019-07-07 15:04:18 +0900

    example link move

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const creds = require('creds')
 const puppeteer = require('puppeteer')
 const express = require('express')
 const app = express()
+
+async function browserGo() {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+    await page.goto('https://example.com')
+}
+browserGo()
 
 app.listen(3000, () => {
     console.log('server connect ok')
