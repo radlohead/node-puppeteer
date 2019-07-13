299ec9cdc6e7767b53254ea5957f20ec26a3cdbb
= <radlohead@naver.com>
2019-07-13 15:05:36 +0900

    불필요한 로직 제거

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const userInfo = require('./userInfo')
 async function browserGo() {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
-    await page.goto('https://www.daum.net/?t__nil_navi=daum')
-    await page.click('.link_login')
+
     await page.goto(
         'https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F'
     )
